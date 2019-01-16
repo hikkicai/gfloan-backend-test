2cc9a690d564fb9a6875506344d7e8162d903a10
apple <apple@HikkisMacBookAir.local>
2019-01-17 00:24:48 +0800

    Add DB codes

--- src/main/resources/StartServer.py
+++ src/main/resources/StartServer.py
@@ -1,19 +1,48 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import pymysql
 
 app = Flask(__name__, template_folder='./')
+db = pymysql.connect("localhost", "root", "root", "calculator_data")
+sql_insert = "INSERT INTO add_data  (user_ip,times,sum) values (%s,1,%s)"
+sql_query = "SELECT times,sum FROM add_data WHERE user_ip=%s"
+sql_update = "UPDATE add_data SET times=%s,sum=%s WHERE user_ip=%s"
+
+
+def update_db(ip, total):
+    with db.cursor() as cur:
+        cur.execute(sql_query, ip)
+        result = cur.fetchone()
+        if not result:
+            cur.execute(sql_insert, (ip, total))
+        else:
+            cur.execute(sql_update, (result[0] + 1, result[1] + total, ip))
+
+        db.commit()
+
+
+def query_db():
+    pass
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    msg = total = ''
     if request.method == "POST":
-        a = request.form['left']
-        b = request.form['right']
-        c = float(a) + float(b)
-        print(type(request))
-        print(type(request.form))
-        return render_template('add.html', RESULT=str(c))
+        x = request.form['num1'].strip()
+        y = request.form['num2'].strip()
+
+        try:
+            total = float(x) + float(y)
+        except ValueError as e:
+            msg = str.format("Invalid data was given: ", e)
+        except Exception as e:
+            msg = str.format("Oops, something went wrong! ", e)
+        else:
+            update_db(request.remote_addr, total)
+
+        return render_template('add.html', RESULT=str(total), MSG=msg)
     return render_template('add.html')
 
 
--- src/test/resources/cucumberTest/CalculatorAPI.feature
+++ src/test/resources/cucumberTest/CalculatorAPI.feature
@@ -1,15 +1,32 @@
 Feature: Calculator restful API test
 
-  Scenario: Verify that calculator is up and running
+  Scenario: Verify that app is up and running
     Given url 'http://127.0.0.1:5000/'
     When method get
     Then status 200
     And match //title == 'Calculator'
 
-  Scenario: Verify that calculator adds numbers correctly
+  Scenario: Verify that app adds 2 numbers correctly
     Given url 'http://127.0.0.1:5000/'
-    And form field left = 5.62
-    And form field right = 6.78
+    And form field num1 = 5.62
+    And form field num2 = 6.78
     When method post
     Then status 200
     And match //*[@name="result"]/span == 12.40
+
+  Scenario: Verify that app handles invalid input properly
+    Given url 'http://127.0.0.1:5000/'
+    And form field num1 = 5.62
+    And form field num2 = 'abc'
+    When method post
+    Then status 200
+    And match //*[@name="result"]/span == ''
+    And match //*[@name="msg"] contains 'Invalid data was given'
+
+  Scenario: Verify that user gets calculation times and sums correctly
+
+  Scenario: Verify that app calculates 3 numbers correctly
+
+  Scenario: Verify that app handles invalid number input
+
+  Scenario: Verify that app handles invalid operator input
